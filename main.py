0376c3581e69960a674abb17759cd68506c834e3
justinham <hanzijun1990@gmail.com>
2021-03-30 22:49:16 -0400

    fir

---
+++ main.py
@@ -0,0 +1,41 @@
+
+####### libs ##############################################
+
+from flask import Flask, render_template, request, make_response, redirect, send_file, Response
+from flask import jsonify, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def MainHandler():
+	return render_template('index.html')
+
+
+@app.route("/index")
+def MainIHandler():
+	return render_template('index.html')
+
+@app.route("/service")
+def MainSHandler():
+	return render_template('services.html')
+
+@app.route("/about")
+def MainAHandler():
+	return render_template('about.html')
+
+@app.route("/contact")
+def MainCHandler():
+	return render_template('contact.html')
+
+###### main #####
+
+if __name__=="__main__":
+
+	app.run(debug=True, host="0.0.0.0", port=80)
+	# app.run(debug=True, host="0.0.0.0", port=4646)
+
+
+
+
+
+
